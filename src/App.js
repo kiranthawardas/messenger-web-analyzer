76ab382b576150b1fb9384c1b0b4fc8a9f77a95f
Kiran Thawardas <kiran@thawardas.com>
2018-12-26 22:48:19 -0600

    initial react commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
+import React from 'react'
+import { Form, ControlLabel } from 'react-bootstrap'
+import axios, { post } from 'axios';
+import { Button, InputGroup, FormControl } from 'react-bootstrap'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+let fileReader;
+const globalStyle = {
+  fontSize: '100%'
+}
+const uploadStyle = {
+  maxWidth: '30rem',
+  marginRight: '1rem'
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      file: null,
+      data: null
+    }
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.handleFileRead = this.handleFileRead.bind(this)
+    this.doSomething = this.doSomething.bind(this);
+  }
+  onFormSubmit(e) {
+    if (this.state.file == null) {
+      alert("no file selected");
+      return;
+    }
+    e.preventDefault();
+    var fr = new FileReader();
+    const _this = this;
+    try {
+      fr.readAsText(this.state.file);
+    }
+    catch (e) {
+      alert("invalid file");
+      return;
+    }
+    fr.onload = function (e) {
+      _this.handleFileRead(e.target.result);
+    };
+  }
+  onChange(e) {
+    this.setState({ file: e.target.files[0] });
+  }
+  handleFileRead(data) {
+    console.log(data);
+    this.setState({ data: JSON.parse(data) }, function () {
+      this.doSomething();
+    });
+  }
+  doSomething() {
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div style={globalStyle}>
+        <form onSubmit={this.onFormSubmit}>
+          <div style={uploadStyle} className="custom-file">
+            <input type="file" className="custom-file-input" id="customFile" onChange={this.onChange}></input>
+            <label className="custom-file-label" htmlFor="customFile">Choose file</label>
+          </div>
+          <button className="btn-primary">Show</button>
+        </form>
+        <p>{JSON.stringify(this.state.data)}</p>
       </div>
-    );
+    )
   }
 }
 
